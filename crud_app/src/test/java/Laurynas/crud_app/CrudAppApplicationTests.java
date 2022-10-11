b7036a9f078d757495f730a436cf731a7a57a1d9
Laurynas <simonaitis.laurynas@gmail.com>
2022-10-11 21:13:45 +0300

    Initial commit

---
+++ crud_app/src/test/java/Laurynas/crud_app/CrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package Laurynas.crud_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
