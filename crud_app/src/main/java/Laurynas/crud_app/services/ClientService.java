4f2d0aa1ae278e0ef77e69d9c8a45a065e8b20b1
Laurynas <simonaitis.laurynas@gmail.com>
2022-10-13 08:56:24 +0300

    described ClientService

--- crud_app/src/main/java/Laurynas/crud_app/services/ClientService.java
+++ crud_app/src/main/java/Laurynas/crud_app/services/ClientService.java
@@ -1,4 +1,32 @@
 package Laurynas.crud_app.services;
 
+import Laurynas.crud_app.entities.Client;
+import Laurynas.crud_app.repositories.ClientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ClientService {
+    private ClientRepository clientRepository;
+
+    public ClientService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    public List<Client> getAllClients(){return this.clientRepository.findAll();}
+
+    public Client getClientById (Long id){
+        Optional <Client> client = this.clientRepository.findById(id);
+        if(client.isPresent()){
+            return client.get();
+        }else
+            return null;
+    }
+
+    public void deleteClientById(Long id) {this.clientRepository.deleteById(id);}
+
+    public void saveClient (Client client){this.clientRepository.save(client);}
+
 }
